1fa9aa3574bb52d419ef19b3da75c1235b94ca83
Clespy13 <49701092+Clespy13@users.noreply.github.com>
2021-04-20 23:34:11 +0200

    Initial commit

---
+++ src/tests_3D/main.cpp
@@ -0,0 +1,62 @@
+#include "testConsoleGameEngine.h"
+#include <vector>
+
+struct vec3d {
+	float x, y, z;
+};
+
+struct triangle {
+	vec3d p[3];
+};
+
+struct mesh {
+	std::vector<triangle> tris;
+};
+
+class test3D : public testConsoleGameEngine {
+public:
+	test3D() {
+		m_sAppName = L"3D test";
+	}
+
+private:
+	mesh MeshCube;
+
+public:
+	bool onUserCreate() override {
+		MeshCube.tris = {
+		// SOUTH
+		{ 0.0f, 0.0f, 0.0f,    0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 0.0f },
+		{ 0.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+
+		// EAST                                                      
+		{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f },
+		{ 1.0f, 0.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 0.0f, 1.0f },
+
+		// NORTH                                                     
+		{ 1.0f, 0.0f, 1.0f,    1.0f, 1.0f, 1.0f,    0.0f, 1.0f, 1.0f },
+		{ 1.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 0.0f, 1.0f },
+
+		// WEST                                                      
+		{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 1.0f,    0.0f, 1.0f, 0.0f },
+		{ 0.0f, 0.0f, 1.0f,    0.0f, 1.0f, 0.0f,    0.0f, 0.0f, 0.0f },
+
+		// TOP                                                       
+		{ 0.0f, 1.0f, 0.0f,    0.0f, 1.0f, 1.0f,    1.0f, 1.0f, 1.0f },
+		{ 0.0f, 1.0f, 0.0f,    1.0f, 1.0f, 1.0f,    1.0f, 1.0f, 0.0f },
+
+		// BOTTOM                                                    
+		{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f },
+		{ 1.0f, 0.0f, 1.0f,    0.0f, 0.0f, 0.0f,    1.0f, 0.0f, 0.0f },
+		}
+
+		return true;
+	};
+};
+
+int main() {
+	test3D demo;
+	if (demo.ConstructConsole(256, 240, 4, 4)) { demo.Start(); }
+
+	return 0;
+}
