c1b24bb3f78310984499d189452e974aa7a1d4b0
Clespy <fclement617@gmail.com>
2023-07-15 11:43:35 +0200

    Creating application class

---
+++ src/Application.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "Window.h"
+
+class Application {
+    public:
+        Application();
+        ~Application();
+
+        void run();
+
+    private:
+        Window m_Window;
+
+}
