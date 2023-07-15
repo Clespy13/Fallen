73e16a5a06f2201eec667e283a788472fa086824
Clespy <fclement617@gmail.com>
2023-07-15 11:43:16 +0200

    Updated main

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,21 @@
-#include <iostream>
+
 //#define GLFW_INCLUDE_NONE
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include "Logger.h"
+#include "Window.h"
 
 int main() {
-    GLFWwindow* window;
+    Window window;
 
-    TRACE("Works %i == 42: %i", 42, 42 == 42);
-    INFO("Works %i == 42: %i", 42, 42 == 42);
-    WARN("Works %i == 42.0: %i", 42, 42 == 42);
-    ERROR("Works %i == 42: %i", 41, 41 == 42);
+    window.CreateWindow("Fallen", 640, 480);
+    //window.SetCallbacks();
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    INFO("Works on windows");
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Fallen", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        std::cout << "Failed to initialize glad" << std::endl;
-        return -1;
-    }
-
-    glViewport(0, 0, 640, 480);
-
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    /* Loop until the user closes the window */ 
+    while (!glfwWindowShouldClose(window.getWindow()))
     {
         glClearColor(0.2f, 0.2f, 0.2f, 0.0f );
 
@@ -45,7 +23,7 @@ int main() {
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(window.getWindow());
 
         /* Poll for and process events */
         glfwPollEvents();
